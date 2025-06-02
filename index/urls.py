c3525f0f4ea8fbe302ac461f701eb3c0eff77a6a
Nicolas Silva <araujo.silva.nicolas@gmail.com>
2025-06-02 16:29:23 -0300

    views and templates removed due to non-reusability and confuseness. Classes' and DB' diagrams need a through review.

--- index/urls.py
+++ index/urls.py
@@ -4,9 +4,5 @@ from index import views
 
 urlpatterns = [
     path('', views.index_view, name='index_page'),
-    path('padarias/', views.padarias_view, name='padarias_page'),
-    path('padarias/<int:padaria_id>/inscrever/', views.inscrever_padaria, name='inscrever_padaria_page'),
-    path('fornadas/', views.fornadas_view, name='fornadas_page'),
-    path('fornadas/cadastrar/', views.cadastrar_fornada, name='cadastrar_fornada_page'),
-    path('notificacoes/', views.notificacoes_view, name='notificacoes_page'),
+    
 ]
